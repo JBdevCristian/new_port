c2e5e8447e92348095c6d61f4860c94f942aae7b
JBdevCristian <jbcristian78@gmail.com>
2024-09-27 15:56:07 -0300

    Finalizando parte de Portifolio

---
+++ projetos/Caco/js/caco.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal({ reset: true })
+
+sr.reveal('.fundos', {duration: 1300})
+
+    function PSS(x) {
+        document.getElementById("BtnPss").className = "btn btn-light mt-3"
+    }
+
+    function PSSOut(x) {
+
+        document.getElementById("BtnPss").className = "btn btn-dark mt-3"
+    }
+
+    function SSP(x) {
+        document.getElementById("BtnSSP").className = "btn btn-warning mt-3"
+    }
+
+    function SSPOut(x) {
+
+        document.getElementById("BtnSSP").className = "btn btn-dark mt-3"
+    }
